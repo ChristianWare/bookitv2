8c616b60f37daef003ea990deefa796fe1880461
Chris Ware <c.p.ware@hotmail.com>
2023-10-17 19:33:03 -0700

    BookIT Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
- export default function Home() {
+import Home from "@/components/Home";
+
+export default function HomePage() {
   return (
     <div className='container'>
-      <h1>Home Page</h1>
+      <Home />
     </div>
   );
 }
