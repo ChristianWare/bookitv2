a5821fd5f5c4fee3bec3bed98811de5dca5df6dc
Chris Ware <c.p.ware@hotmail.com>
2023-10-17 19:08:15 -0700

    Header & Footer Component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/css/bootstrap.css";
 
 import { Globalprovider } from "@/GlobalProvider";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Script from "next/script";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +23,11 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <Globalprovider>{children}</Globalprovider>
+        <Globalprovider>
+          <Header />
+          {children}
+          <Footer />
+        </Globalprovider>
 
         <Script src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js'></Script>
         <Script src='https://kit.fontawesome.com/b6b7b8a602.js'></Script>
