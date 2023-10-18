0dfe1a9e6fb84c1a348265d78e412b58aea7b0a9
Chris Ware <c.p.ware@hotmail.com>
2023-10-18 12:25:17 -0700

    Data Caching + fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 import Home from "@/components/Home";
 
-export default function HomePage() {
+const getRooms = async () => {
+  const res = await fetch("http://localhost:3000/api/rooms", {
+    cache: "no-store",
+  });
+  return res.json();
+};
+
+export default async function HomePage() {
+  const rooms = await getRooms();
+  console.log("resPerPage =>", rooms.resPerPage);
   return (
     <div className='container'>
       <Home />
--- backend/controllers/roomControllers.ts
+++ backend/controllers/roomControllers.ts
@@ -16,7 +16,6 @@ export const allRoooms = catchAsycnErrors(async (req: NextRequest) => {
     queryStr[key] = value;
   });
 
-  const roomsCount: number = await Room.countDocuments();
 
   const apiFilters = new APIFilters(Room, queryStr).search().filter();
 
@@ -28,7 +27,6 @@ export const allRoooms = catchAsycnErrors(async (req: NextRequest) => {
 
   return NextResponse.json({
     success: true,
-    roomsCount,
     filteredRoomsCount,
     resPerPage,
     rooms,
