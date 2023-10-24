70f18475f78bbeed7a02e6c1c887a7142ff6f832
Chris Ware <c.p.ware@hotmail.com>
2023-10-24 16:15:53 -0700

    Loading Component

--- components/Home.tsx
+++ components/Home.tsx
@@ -19,9 +19,9 @@ const Home = ({ data }: Props) => {
     <div>
       <section id='rooms' className='container mt-5'>
         <h2 className='mb-3 ml-2 stays-heading'>All Rooms</h2>
-        <a href='/search' className='ml-2 back-to-search'>
+        {/* <a href='/search' className='ml-2 back-to-search'>
           <i className='fa fa-arrow-left'></i> Back to Search
-        </a>
+        </a> */}
         <div className='row mt-4'>
           {rooms?.length === 0 ? (
             <div className='alret alret-danger mt-5 w-100'>
