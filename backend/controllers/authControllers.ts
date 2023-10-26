ff6106853afb43bad763cd646e39b40330605d91
Chris Ware <c.p.ware@hotmail.com>
2023-10-26 13:20:02 -0700

    Update User Profile Endpoint

--- backend/controllers/authControllers.ts
+++ backend/controllers/authControllers.ts
@@ -18,3 +18,20 @@ export const registerUser = catchAsycnErrors(async (req: NextRequest) => {
     success: true,
   });
 });
+
+// Update User Profile => /api/me/update
+export const updateProfile = catchAsycnErrors(async (req: NextRequest) => {
+  const body = await req.json();
+
+  const userData = {
+    name: body.name,
+    email: body.email,
+  };
+
+  const user = await User.findByIdAndUpdate(req.user._id, userData);
+
+  return NextResponse.json({
+    success: true,
+    user,
+  });
+});
