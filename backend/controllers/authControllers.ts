f51d33fca3a0702797097e9fe179aa5e77669f8d
Chris Ware <c.p.ware@hotmail.com>
2023-10-27 17:32:23 -0700

    Upload User Avatar Endpoint

--- backend/controllers/authControllers.ts
+++ backend/controllers/authControllers.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { catchAsycnErrors } from "../middlewares/catchAsyncErrors";
 import User from "../models/user";
 import ErrorHandler from "../utils/errorHandler";
+import { delete_file, upload_file } from "../utils/cloudinary";
 
 // Register User => /api/auth/register
 export const registerUser = catchAsycnErrors(async (req: NextRequest) => {
@@ -54,5 +55,26 @@ export const updatePassword = catchAsycnErrors(async (req: NextRequest) => {
 
   return NextResponse.json({
     success: true,
+  });
+});
+
+// Update user avatar => /api/me/upload_avatar
+export const uploadAvatar = catchAsycnErrors(async (req: NextRequest) => {
+  const body = await req.json();
+
+  const avatarResponse = await upload_file(body?.avatar, "bookit/avatars");
+
+  // Remove avatar from cloudinary
+  if (req?.user?.avatar?.public_id) {
+    await delete_file(req?.user?.avatar?.public_id);
+  }
+
+  const user = await User.findByIdAndUpdate(req?.user?._id, {
+    avatar: avatarResponse,
+  });
+
+  return NextResponse.json({
+    success: true,
+    user,
   });
 });
