388d6777b402ccd26f8ffd7af48705b25d57110e
Chris Ware <c.p.ware@hotmail.com>
2023-11-07 12:03:50 -0700

    get rooms api update

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,9 @@ export const revalidate = 0;
 
 const getRooms = async () => {
   try {
-    const res = await fetch(`${process.env.API_URI}/api/rooms`);
+    const res = await fetch(
+      "https://bookitv2-christianware.vercel.app/api/rooms"
+    );
     const data = res.json();
     return data;
   } catch (error) {
